4eb8b405681f8743ba9a4ae7e5d46488c6d8ae6e
Leonel <gobbileonel@gmail.com>
2019-06-08 21:22:45 -0300

    UNT-3 update prefabs and add audiosource

--- 3_project_boost/3_Project_Boost/Assets/Rocket.cs
+++ 3_project_boost/3_Project_Boost/Assets/Rocket.cs
@@ -3,11 +3,13 @@
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,14 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();    
         }
 
         if (Input.GetKey(KeyCode.A))
